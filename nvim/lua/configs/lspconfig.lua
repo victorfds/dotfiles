81a4621407b51e147610047f58ec98a380dc703b
Victor Ferreira Da Silva <victor.ferreira@castgroup.com.br>
2025-09-14 22:05:12 -0400

    chore: nvim lspconfig updated

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -88,11 +88,51 @@ lspconfig.rust_analyzer.setup {
 local mason_registry = require "mason-registry"
 
 -- Get the installation path for Vue Language Server
-local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+local vue_language_server_path = vim.fn.expand "$MASON/packages"
+  .. "/vue-language-server"
   .. "/node_modules/@vue/language-server"
 
-local typescript_server_path = mason_registry.get_package("typescript-language-server"):get_install_path()
-  .. "/node_modules/typescript/lib"
+-- local typescript_server_path = vim.fn.expand(
+--   "$MASON/packages" .. "/typescript-language-server" .. "/node_modules/typescript/lib"
+-- ) .. "/node_modules/typescript/lib"
+
+local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+local vue_plugin = {
+  name = "@vue/typescript-plugin",
+  location = vue_language_server_path,
+  languages = { "vue" },
+  configNamespace = "typescript",
+}
+local vtsls_config = {
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          vue_plugin,
+        },
+      },
+    },
+  },
+  filetypes = tsserver_filetypes,
+}
+
+local ts_ls_config = {
+  init_options = {
+    plugins = {
+      vue_plugin,
+    },
+  },
+  filetypes = tsserver_filetypes,
+}
+
+-- If you are on most recent `nvim-lspconfig`
+local vue_ls_config = {}
+
+-- nvim 0.11 or above
+vim.lsp.config("vtsls", vtsls_config)
+vim.lsp.config("vue_ls", vue_ls_config)
+vim.lsp.config("ts_ls", ts_ls_config)
+vim.lsp.enable { "vtsls", "vue_ls" } -- If using `ts_ls` replace `vtsls` to `ts_ls`
 
 -- Tailwind CSS LSP setup
 lspconfig.tailwindcss.setup {
@@ -124,81 +164,4 @@ lspconfig.tailwindcss.setup {
   env = {
     NODE_OPTIONS = "--max-old-space-size=4096",
   },
-}
-
--- TypeScript Language Server with Vue plugin
-lspconfig.ts_ls.setup {
-  init_options = {
-    plugins = {
-      {
-        name = "@vue/typescript-plugin",
-        location = vue_language_server_path,
-        languages = { "javascript", "typescript" },
-      },
-    },
-  },
-  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
-  on_attach = on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-  settings = {
-    typescript = {
-      inlayHints = {
-        includeInlayParameterNameHints = "all",
-        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = false,
-        includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
-      },
-      format = {
-        printWidth = 80,
-      },
-    },
-  },
-}
-
--- Volar for Vue
-lspconfig.volar.setup {
-  filetypes = { "vue" },
-  init_options = {
-    typescript = {
-      tsdk = typescript_server_path,
-    },
-    vue = {
-      hybridMode = false,
-    },
-    documentFeatures = {
-      documentFormatting = {
-        defaultPrintWidth = 100,
-      },
-    },
-  },
-  settings = {
-    typescript = {
-      inlayHints = {
-        enumMemberValues = {
-          enabled = true,
-        },
-        functionLikeReturnTypes = {
-          enabled = true,
-        },
-        propertyDeclarationTypes = {
-          enabled = true,
-        },
-        parameterTypes = {
-          enabled = true,
-          suppressWhenArgumentMatchesName = true,
-        },
-        variableTypes = {
-          enabled = false,
-        },
-      },
-    },
-  },
-  on_attach = on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
 }
