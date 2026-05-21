64c29d48f6c56a832e881250edc492ecd8618226
Victor Ferreira Da Silva <victor.ferreira@castgroup.com.br>
2026-05-21 18:07:50 -0400

    feat: update weather location and enhance hypridle configuration

--- lvim/config.lua
+++ lvim/config.lua
@@ -3,148 +3,246 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
+-- Compatibility shims for running LunarVim 1.4-era plugins on Neovim 0.12+.
+if vim.fn.has("nvim-0.12") == 1 then
+  local suppressed_deprecations = {
+    ["client.request"] = true,
+    ["client.supports_method"] = true,
+    ["vim.validate{<table>}"] = true,
+    ["vim.lsp.for_each_buffer_client()"] = true,
+    ["vim.lsp.start_client()"] = true,
+  }
+  local original_deprecate = vim.deprecate
+  vim.deprecate = function(name, alternative, version, plugin, backtrace)
+    if suppressed_deprecations[name] then
+      return
+    end
+
+    return original_deprecate(name, alternative, version, plugin, backtrace)
+  end
+
+  if vim.islist ~= nil then
+    vim.tbl_islist = vim.islist
+  end
+
+  vim.lsp.with = function(handler, override_config)
+    return function(err, result, ctx, config)
+      return handler(err, result, ctx, vim.tbl_deep_extend("force", config or {}, override_config))
+    end
+  end
+
+  vim.tbl_flatten = function(tbl)
+    local result = {}
+
+    local function flatten(value)
+      for index = 1, #value do
+        local item = value[index]
+        if type(item) == "table" then
+          flatten(item)
+        elseif item then
+          table.insert(result, item)
+        end
+      end
+    end
+
+    flatten(tbl)
+    return result
+  end
+
+  vim.tbl_add_reverse_lookup = function(tbl)
+    for key, value in pairs(tbl) do
+      tbl[value] = key
+    end
+
+    return tbl
+  end
+
+  if vim.diagnostic.is_disabled == nil and vim.diagnostic.is_enabled ~= nil then
+    vim.diagnostic.is_disabled = function(bufnr, namespace)
+      return not vim.diagnostic.is_enabled({ bufnr = bufnr, ns_id = namespace })
+    end
+  end
+
+  if vim.diagnostic.disable == nil and vim.diagnostic.enable ~= nil then
+    vim.diagnostic.disable = function(bufnr, namespace)
+      vim.diagnostic.enable(false, { bufnr = bufnr, ns_id = namespace })
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TSUpdate",
+  callback = function()
+    require("nvim-treesitter.parsers").rust_with_rstml = {
+      install_info = {
+        url = "https://github.com/rayliwell/tree-sitter-rstml",
+        branch = "main",
+        files = { "src/parser.c", "src/scanner.c" },
+        location = "rust_with_rstml",
+      },
+    }
+  end,
+})
+vim.treesitter.language.register("rust_with_rstml", { "rust" })
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
+  "css",
+  "html",
+  "javascript",
+  "json",
   "lua",
+  "markdown",
+  "markdown_inline",
+  "query",
   "rust",
+  "rust_with_rstml",
+  "scss",
+  "tsx",
   "toml",
-  "vue"
+  "typescript",
+  "vimdoc",
+  "vue",
 }
--- Mason and lspconfig setup
-require('mason').setup()
-require('mason-lspconfig').setup {
-  ensure_installed = {
-    'volar',
-    'tsserver',
-  },
+-- Let LunarVim initialize Mason, and only provide the servers we want installed.
+lvim.lsp.installer.setup.ensure_installed = {
+  "rust_analyzer",
+  "ts_ls",
+  "vue_ls",
 }
 
--- Require lspconfig
-local lspconfig = require('lspconfig')
-local mason_registry = require('mason-registry')
+lvim.lsp.installer.setup.automatic_installation = {
+  exclude = {},
+}
 
--- Get the installation path for Vue Language Server
-local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
--- local vue_language_server_path = "/home/victor/.nvm/versions/node/v20.17.0/lib/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin"
+local function get_mason_package_install_path(package_name)
+  local package_path = vim.fn.stdpath("data") .. "/mason/packages/" .. package_name
+  if vim.fn.isdirectory(package_path) == 0 then
+    return nil
+  end
 
-lspconfig.tsserver.setup {
-  init_options = {
+  return package_path
+end
+
+local lspconfig_runtime_path = vim.env.LUNARVIM_RUNTIME_DIR .. "/site/pack/lazy/opt/nvim-lspconfig"
+if vim.fn.isdirectory(lspconfig_runtime_path) == 1 then
+  vim.opt.rtp:prepend(lspconfig_runtime_path)
+end
+
+local function mason_bin_path(binary_name)
+  local path = vim.fn.stdpath("data") .. "/mason/bin/" .. binary_name
+  if vim.fn.executable(path) == 1 then
+    return path
+  end
+
+  return binary_name
+end
+
+local tsserver_init_options = {}
+local vue_language_server_install_path = get_mason_package_install_path("vue-language-server")
+
+if vue_language_server_install_path ~= nil then
+  tsserver_init_options = {
     plugins = {
       {
-        name = '@vue/typescript-plugin',
-        location = vue_language_server_path,
+        name = "@vue/typescript-plugin",
+        location = vue_language_server_install_path .. "/node_modules/@vue/language-server",
         languages = { "vue" },
       },
     },
-  },
-  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-}
+  }
+end
 
--- No need to set `hybridMode` to `true` as it's the default value
-lspconfig.volar.setup {}
+vim.lsp.config("ts_ls", {
+  cmd = { mason_bin_path("typescript-language-server"), "--stdio" },
+  init_options = tsserver_init_options,
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+})
+vim.lsp.enable("ts_ls")
 
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
+-- No need to set `hybridMode` to `true` as it's the default value.
+vim.lsp.config("vue_ls", {
+  cmd = { mason_bin_path("vue-language-server"), "--stdio" },
+  filetypes = { "vue" },
+})
+vim.lsp.enable("vue_ls")
+
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, {
+  "rust_analyzer",
+  "ts_ls",
+  "tsserver",
+  "vue_ls",
+  "volar",
+})
 
 local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
 
 local codelldb_path = mason_path .. "bin/codelldb"
-local liblldb_path = mason_path .. "packages/codelldb/extension/lldb/lib/liblldb"
-local this_os = vim.loop.os_uname().sysname
+local this_os = vim.uv.os_uname().sysname
 
 -- The path in windows is different
 if this_os:find "Windows" then
   codelldb_path = mason_path .. "packages\\codelldb\\extension\\adapter\\codelldb.exe"
-  liblldb_path = mason_path .. "packages\\codelldb\\extension\\lldb\\bin\\liblldb.dll"
-else
-  -- The liblldb extension is .so for linux and .dylib for macOS
-  liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
 end
 
-pcall(function()
-  require("rust-tools").setup {
-    tools = {
-      executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
-      reload_workspace_from_cargo_toml = true,
-      runnables = {
-        use_telescope = true,
-      },
-      inlay_hints = {
-        auto = true,
-        only_current_line = false,
-        show_parameter_hints = false,
-        parameter_hints_prefix = "<-",
-        other_hints_prefix = "=>",
-        max_len_align = false,
-        max_len_align_padding = 1,
-        right_align = false,
-        right_align_padding = 7,
-        highlight = "Comment",
-      },
-      hover_actions = {
-        border = "rounded",
-      },
-      on_initialized = function()
-        vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
-          pattern = { "*.rs" },
-          callback = function()
-            local _, _ = pcall(vim.lsp.codelens.refresh)
-          end,
-        })
-      end,
+local rust_settings = {
+  ["rust-analyzer"] = {
+    lens = {
+      enable = true,
+      references = true,
+      implementations = true,
+      enumVariantReferences = true,
+      methodReferences = true,
     },
-    dap = {
-      -- adapter= codelldb_adapter,
-      adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+    checkOnSave = {
+      allFeatures = true,
+      enable = true,
+      command = "clippy",
     },
-    server = {
-      on_attach = function(client, bufnr)
-        require("lvim.lsp").common_on_attach(client, bufnr)
-        local rt = require "rust-tools"
-        vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
-      end,
-
-      capabilities = require("lvim.lsp").common_capabilities(),
-      settings = {
-        ["rust-analyzer"] = {
-          lens = {
-            enable = true,
-            references = true,
-            implementations = true,
-            enumVariantReferences = true,
-            methodReferences = true
-          },
-          checkOnSave = {
-            allFeatures = true,
-            enable = true,
-            command = "clippy",
-          },
-          procMacro = {
-            enable = true,
-            ignored = {
-              leptosMacro = {
-                "server"
-              }
-            }
-          },
-          cargo = {
-            allFeatures = true,
-            autoreload = true
-          },
-          rustfmt = {
-            overrideCommand = { "leptosfmt", "--stdin", "--rustfmt" }
-          },
-          callInfo = { full = true },
-          inlayHints = { enable = true, typeHints = true, parameterHints = true },
-          hoverActions = { enable = true }
+    procMacro = {
+      enable = true,
+      ignored = {
+        leptosMacro = {
+          "server",
         },
       },
     },
-  }
-end)
+    cargo = {
+      allFeatures = true,
+      autoreload = true,
+    },
+    rustfmt = {
+      overrideCommand = { "leptosfmt", "--stdin", "--rustfmt" },
+    },
+    callInfo = { full = true },
+    inlayHints = { enable = true, typeHints = true, parameterHints = true },
+  },
+}
+
+vim.lsp.config("rust_analyzer", {
+  cmd = { mason_bin_path("rust-analyzer") },
+  on_attach = function(client, bufnr)
+    require("lvim.lsp").common_on_attach(client, bufnr)
+    vim.keymap.set("n", "K", function()
+      if #vim.lsp.get_clients({ bufnr = bufnr, method = "textDocument/hover" }) > 0 then
+        vim.lsp.buf.hover()
+      end
+    end, { buffer = bufnr, desc = "LSP hover" })
+  end,
+  capabilities = require("lvim.lsp").common_capabilities(),
+  settings = rust_settings,
+})
+vim.lsp.enable("rust_analyzer")
 
 lvim.builtin.dap.on_config_done = function(dap)
-  dap.adapters.codelldb = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+  dap.adapters.codelldb = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      command = codelldb_path,
+      args = { "--port", "${port}" },
+    },
+  }
   dap.configurations.rust = {
     {
       name = "Launch file",
@@ -159,22 +257,13 @@ lvim.builtin.dap.on_config_done = function(dap)
   }
 end
 
-vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<m-d>", vim.lsp.buf.hover, { desc = "LSP hover" })
 
 lvim.builtin.which_key.mappings["C"] = {
   name = "Rust",
-  r = { "<cmd>RustRunnables<Cr>", "Runnables" },
   t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
-  m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
-  c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
-  p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
-  d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
-  v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
-  R = {
-    "<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
-    "Reload Workspace",
-  },
-  o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
+  a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+  h = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover" },
   y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
   P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
   i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
@@ -183,16 +272,23 @@ lvim.builtin.which_key.mappings["C"] = {
 }
 local leet_arg = "leetcode.nvim"
 lvim.plugins = {
-  "simrat39/rust-tools.nvim",
+  {
+    "echasnovski/mini.icons",
+    version = false,
+    config = function()
+      require("mini.icons").setup()
+    end,
+  },
   {
     "saecki/crates.nvim",
-    version = "v0.3.0",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("crates").setup {
-        null_ls = {
+        lsp = {
           enabled = true,
-          name = "crates.nvim",
+          actions = true,
+          completion = true,
+          hover = true,
         },
         popup = {
           border = "rounded",
@@ -232,24 +328,18 @@ lvim.plugins = {
     },
     ft = { "fugitive" }
   },
-  -- {
-  --   "windwp/nvim-ts-autotag",
-  --   config = function()
-  --     require("nvim-ts-autotag").setup()
-  --   end,
-  -- },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
   {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = true
     -- use opts = {} for passing setup options
     -- this is equalent to setup({}) function
-  },
-  {
-    "rayliwell/nvim-ts-autotag",
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end,
   },
   {
     "rayliwell/tree-sitter-rstml",
@@ -263,39 +353,6 @@ lvim.plugins = {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
   },
-  {
-   "kawre/leetcode.nvim",
-    lazy = leet_arg ~= vim.fn.argv(0, -1),
-    opts = { arg = leet_arg },
-    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        -- "ibhagwan/fzf-lua",
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-    },
-    opts = {
-        -- configuration goes here
-      lang = "rust"
-    },
-    hooks = {
-      ---@type fun(question: lc.ui.Question)[]
-      ["question_enter"] = {
-        function()
-          -- os.execute "sleep 1"
-          local file_extension = vim.fn.expand "%:e"
-          if file_extension == "rs" then
-            local bash_script = tostring(vim.fn.stdpath "data" .. "/leetcode/rust_init.sh")
-            local success, error_message = os.execute(bash_script)
-            if success then
-              print "Successfully updated rust-project.json"
-              vim.cmd "LspRestart rust_analyzer"
-            else
-              print("Failed update rust-project.json. Error: " .. error_message)
-            end
-          end
-        end,
-      },
-    }
-  }
+  { "nvim-tree/nvim-web-devicons", opts = {} },
+  
 }
